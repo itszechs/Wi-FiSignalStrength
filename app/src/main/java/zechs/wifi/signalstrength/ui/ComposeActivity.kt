d2ff92f9db406875325d8c3576629195e1692a23
zechs <vijay.vky2002@gmail.com>
2022-02-20 16:55:56 +0530

    Refracted code & added amoled dark theme

--- app/src/main/java/zechs/wifi/signalstrength/ui/ComposeActivity.kt
+++ app/src/main/java/zechs/wifi/signalstrength/ui/ComposeActivity.kt
@@ -3,6 +3,7 @@ package zechs.wifi.signalstrength.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,29 +14,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import zechs.wifi.signalstrength.repository.MainRepository
 import zechs.wifi.signalstrength.ui.theme.WiFiSignalStrengthTheme
 import zechs.wifi.signalstrength.viewmodel.MainViewModel
-import zechs.wifi.signalstrength.viewmodel.MainViewModelProviderFactory
 
-
-@InternalCoroutinesApi
 class ComposeActivity : ComponentActivity() {
 
-    private lateinit var mainViewModel: MainViewModel
+    private val mainViewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val mainRepository = MainRepository()
-        val viewModelProviderFactory = MainViewModelProviderFactory(mainRepository)
-        mainViewModel =
-            ViewModelProvider(this, viewModelProviderFactory).get(MainViewModel::class.java)
 
         setContent {
             WiFiSignalStrengthTheme {
@@ -47,13 +36,11 @@ class ComposeActivity : ComponentActivity() {
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     var strengthValue by remember { mutableStateOf(0) }
-
                     lifecycleScope.launch {
-                        mainViewModel.getSignal().collect {
+                        mainViewModel.fetchSignalStrength().collect {
                             strengthValue = it
                         }
                     }
-
                     CustomComponent(indicatorValue = strengthValue)
                 }
             }
--- app/src/main/java/zechs/wifi/signalstrength/viewmodel/MainViewModel.kt
+++ app/src/main/java/zechs/wifi/signalstrength/viewmodel/MainViewModel.kt
@@ -1,15 +1,33 @@
 package zechs.wifi.signalstrength.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Activity
+import android.app.Application
+import android.net.wifi.WifiManager
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import zechs.wifi.signalstrength.repository.MainRepository
+import kotlinx.coroutines.flow.flow
 
-class MainViewModel(
-    private val mainRepository: MainRepository
-) : ViewModel() {
+class MainViewModel(app: Application) : AndroidViewModel(app) {
 
-    fun getSignal(): Flow<Int> {
-        return mainRepository.fetchSignalStrength()
+    private val context = getApplication<Application>()
+    private val wifiManager = context.getSystemService(
+        Activity.WIFI_SERVICE
+    ) as WifiManager
+
+    fun fetchSignalStrength(): Flow<Int> = flow {
+        while (true) {
+            val dbm = wifiManager.connectionInfo.rssi
+            val percent = 2 * (dbm + 100)
+            val finalPercent = when {
+                percent >= 100 -> 100
+                percent <= 0 -> 0
+                else -> percent
+            }
+            emit(finalPercent)
+            Log.d("fetchSignalFlow", "percent=$finalPercent")
+            delay(500L)
+        }
     }
-
 }
